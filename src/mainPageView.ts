02a01c03be44f3abd4e43a1040143b193a729b63
Little Whale <vladimirputin420@protonmail.com>
2020-11-02 16:48:01 +0300

    styles applyed

--- src/mainPageView.ts
+++ src/mainPageView.ts
@@ -1,12 +1,12 @@
 import { arrayOfParts } from "./index"
 import { createElement } from "./htmlUtils"
 import { backgroundUrl, fetGetRequest, fullUrl, getImagesUrls, getRandomLink, getUnusedPartsOfFullImage, getPartsUrl, searchByFullImg, compareArrays, randomFromRange } from "./utils"
-import { resolve } from "path";
+import { resolve } from "path"
 
 export async function renderMainPage(rootElement: HTMLElement, backgroundUrl: string) {
-    // const headerName = "header"
-    // const artefact = "1488"
-    // createHeader(headerName, artefact)
+    const headerName = "header"
+    const artefact = "1488"
+    createHeader(headerName, artefact)
     // return Promise.all([createBackground(rootElement, backgroundUrl), createImageFull(rootElement, fullUrl)])
 
     const arrayOfFullImages = await getImagesUrls(fullUrl)
@@ -17,12 +17,12 @@ export async function renderMainPage(rootElement: HTMLElement, backgroundUrl: st
         searchByFullImg(arrayOfParts, singleFullImageUrl)
             .map((url) => `parts/${url}`)
     const firstPart = projectParts[0]
-    const restOfTheParts = projectParts.slice(1);
+    const restOfTheParts = projectParts.slice(1)
 
     while (true) {
-        await waitSinglePartStateSwitch(rootElement, firstPart);
-        await waitMultiPartStateSwitch(rootElement, restOfTheParts);
-        await waitFullPageStateSwith(rootElement, projectFullImageUrl);
+        await waitSinglePartStateSwitch(rootElement, firstPart)
+        await waitMultiPartStateSwitch(rootElement, restOfTheParts)
+        await waitFullPageStateSwith(rootElement, projectFullImageUrl)
     }
 }
 
@@ -42,35 +42,46 @@ async function waitSinglePartStateSwitch(rootElement: HTMLElement, singlePartUrl
 async function waitMultiPartStateSwitch(rootElement: HTMLElement, partsUrls: string[]) {
     createBackground(rootElement, backgroundUrl)
 
-    const minRadiusPx = 280;
-    const maxRadiusPx = 320;
-    const minAngleRad = 0;
-    const maxAngleRad = Math.PI * 2;
+    const minRadiusPx = 180
+    const maxRadiusPx = 200
+    const minAngleRad = 0
+    const maxAngleRad = Math.PI * 2
     return new Promise((resolve) => {
         partsUrls.forEach(partUrl => {
-            const radiusPx = Math.round(randomFromRange(minRadiusPx, maxRadiusPx));
+            const radiusPx = Math.round(randomFromRange(minRadiusPx, maxRadiusPx))
             console.log(radiusPx)
-            const angleRad = randomFromRange(minAngleRad, maxAngleRad);
-            const partImg = createPartImage(partUrl, radiusPx, angleRad);
+            const angleRad = randomFromRange(minAngleRad, maxAngleRad)
+            const partImg = createPartImage(partUrl, radiusPx, angleRad)
             partImg.addEventListener("click", () => {
-                resolve();
+                resolve()
             })
             rootElement.appendChild(partImg)
         })
-    });
+    })
 }
 
 async function waitFullPageStateSwith(rootElement: HTMLElement, singleImageUrl: string) {
     rootElement.innerHTML = ""
     createBackground(rootElement, backgroundUrl)
-    rootElement.appendChild(createPartImage(singleImageUrl, 0, 0))
-    return new Promise((resolve) => null);
+    const fullImg = createPartImage(singleImageUrl, 0, 0)
+    fullImg.setAttribute("class", "fullImg")
+    rootElement.appendChild(fullImg)
+    return new Promise((resolve) => null)
 }
 
 
 const createHeader = (headerName: string, artefact: string) => {
-    const header = document.getElementsByClassName(`${headerName}`)
-    header[0].innerHTML = `Background / artefact: ${artefact}`
+    const header = document.getElementById(`${headerName}`)
+    const bgH1 = createElement("h1")
+    const artH1 = createElement("h1")
+    const artNumH1 = createElement("h1", "artefact")
+    bgH1.innerText = "Background / "
+    artH1.innerText = "artefact"
+    artNumH1.innerText = `${artefact}`
+    bgH1.setAttribute("class", "bgH1")
+    artH1.setAttribute("class", "artH1")
+    artNumH1.setAttribute("class", "artNumH1")
+    header.append(bgH1, artH1, artNumH1)
 }
 
 async function createBackground(rootElement: HTMLElement, backgroundUrl: string): Promise<void> {
@@ -92,8 +103,8 @@ function createPartImage(imageUrl: string, radiusPx: number, angleRad: number): 
     const partOfImg = createElement("img")
     partOfImg.src = imageUrl
     partOfImg.setAttribute("class", "partImg")
-    const xShift = Math.cos(angleRad) * radiusPx;
-    const yShift = Math.sin(angleRad) * radiusPx;
+    const xShift = Math.cos(angleRad) * radiusPx
+    const yShift = Math.sin(angleRad) * radiusPx
     partOfImg.style = `transform: translate(-50%, -50%) translate(${xShift}px, ${yShift}px)`
-    return partOfImg;
+    return partOfImg
 }
